0ef3a69f27423bed24a7241adf67c560ea19b819
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-05-04 19:34:54 +0900

    [Silver III] Title: 두 스티커, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16937. 두 스티커/두 스티커.cc
+++ 백준/Silver/16937. 두 스티커/두 스티커.cc
@@ -4,57 +4,62 @@
 
 using namespace std;
 
-struct Sticker {
-    int width, height;
-};
+int h, w, n, r, c;
 
-// 스티커를 회전시키는 함수
-vector<pair<int, int>> getRotations(Sticker& sticker) {
-    vector<pair<int, int>> rotations;
-    // 원래 상태와 90도 회전 상태 추가
-    rotations.push_back({ sticker.height, sticker.width });
-    rotations.push_back({ sticker.width, sticker.height });
-    return rotations;
+vector<pair<int, int>> sticker;
+int answer;
+
+
+vector<pair<int, int>>rotate(pair<int, int> sticker) {
+	
+	vector<pair<int, int>> rotation;
+
+	rotation.push_back({ sticker.first, sticker.second });
+	rotation.push_back({ sticker.second, sticker.first });
+
+	return rotation;
 }
 
-// 모눈종이에 스티커 두 개를 붙일 수 있는 최대 넓이를 계산하는 함수
-int getMaxArea(int H, int W, vector<Sticker>& stickers) {
-    int maxArea = 0;
+int getMaxArea() {
+	
+	int maxArea = 0;
 
-    // 모든 스티커 쌍에 대해 루프
-    for (int i = 0; i < stickers.size(); i++) {
-        for (int j = i + 1; j < stickers.size(); j++) {
-            // 각 스티커에 대해 가능한 회전을 구함
-            auto rotations1 = getRotations(stickers[i]);
-            auto rotations2 = getRotations(stickers[j]);
+	for (int i = 0; i < sticker.size(); i++) {
+		for (int j = i + 1; j < sticker.size(); j++) {
+			
+			vector<pair<int, int>> first = rotate(sticker[i]);
+			vector<pair<int, int>> second = rotate(sticker[j]);
 
-            // 가능한 회전 조합에 대해 루프
-            for (auto& r1 : rotations1) {
-                for (auto& r2 : rotations2) {
-                    // 각 회전 상태에서 두 스티커를 배치할 수 있는지 확인
-                    if ((r1.first + r2.first <= H && max(r1.second, r2.second) <= W) ||
-                        (r1.second + r2.second <= W && max(r1.first, r2.first) <= H)) {
-                        maxArea = max(maxArea, r1.first * r1.second + r2.first * r2.second);
-                    }
-                }
-            }
-        }
-    }
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 2; j++) {
+					// 가로 방향으로 두 스티커를 배치할 수 있는 경우
+					if (first[i].first + second[j].first <= w && max(first[i].second, second[j].second) <= h) {
+						maxArea = max(maxArea, first[i].first * first[i].second + second[j].first * second[j].second);
+					}
+					// 세로 방향으로 두 스티커를 배치할 수 있는 경우
+					if (max(first[i].first, second[j].first) <= w && first[i].second + second[j].second <= h) {
+						maxArea = max(maxArea, first[i].first * first[i].second + second[j].first * second[j].second);
+					}
+				}
+			}
+		}
+	}
 
-    return maxArea;
+	return maxArea;
 }
 
-int main() {
-    int H, W, N;
-    cin >> H >> W;
-    cin >> N;
-    vector<Sticker> stickers(N);
 
-    for (int i = 0; i < N; i++) {
-        cin >> stickers[i].height >> stickers[i].width;
-    }
+int main(void) {
+	
+	cin >> h >> w;
+	cin >> n;
 
-    cout << getMaxArea(H, W, stickers) << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> r >> c;
+		sticker.push_back({ r, c });
+	}
 
-    return 0;
+	cout << getMaxArea();
+
+	return 0;
 }
