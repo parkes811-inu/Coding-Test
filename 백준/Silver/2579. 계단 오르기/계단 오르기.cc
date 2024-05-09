67c5302545bbed7bc090b33658c6c11fdfb26787
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-05-09 17:12:34 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> stair(n + 1, 0);  // 계단 점수
+    vector<int> dp(n + 1, 0);     // 최대 점수 저장
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> stair[i];
+    }
+
+    // 초기화
+    dp[1] = stair[1];
+
+    if (n >= 2) {
+        dp[2] = stair[1] + stair[2];
+    }
+
+    // 점화식
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
+    }
+
+    cout << dp[n];
+    return 0;
+}
